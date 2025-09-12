00edff3b2b659bdcf82d7e61092a9c9dde6c93ec
aaron calkins <aaron.calkins123@gmail.com>
2025-09-12 12:58:02 -0400

    final

--- MERN_CodeBloggs_ForFrontend/MERN_CodeBloggs_ForFrontend/client/src/components/Register.jsx
+++ MERN_CodeBloggs_ForFrontend/MERN_CodeBloggs_ForFrontend/client/src/components/Register.jsx
@@ -142,6 +142,7 @@ export default function Register() {
       {error && <Alert variant="danger">{error}</Alert>}
 
       <button type="submit" className="btn btn-primary w-100">Register</button>
+      <a href="/login" className="d-block text-center mt-3">Already have an account? Login here.</a>
     </form>
   );
 }
